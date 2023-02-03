f69eef538cbb5f6eddb213405e108f2e9e9b4377
Pradeep Singh <88311782+gprs1022@users.noreply.github.com>
2023-02-03 20:26:43 +0530

    Download and upload button action

---
+++ app/src/main/java/com/example/downlodeuplodeapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.downlodeuplodeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonUpload = findViewById<Button>(R.id.btnUpload)
+        val buttonDownload = findViewById<Button>(R.id.btnDownload)
+
+        buttonUpload.setOnClickListener {
+
+            Toast.makeText(applicationContext, "Uploading...", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Downloading...", Toast.LENGTH_SHORT).show(
+        }
+        buttonDownload.setOnClickListener {
+)
+        }
+    }
+}
